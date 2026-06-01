399ad279d58670cfec15b46011e05718021dde5a
Luke Blommesteyn <lblommesteyn@icloud.com>
2026-06-01 03:44:21 -0400

    fix: clean up pnc backend bringup

--- src/bringup/launch/pnc_backend.launch.py
+++ src/bringup/launch/pnc_backend.launch.py
@@ -7,7 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 '''
 Run this file initially with ros2 launch ap1_bringup pnc_backend.launch.py 
 
-it will get planning, control and the sim up and running 
+it will get planning and control up and running
 '''
 
 def generate_launch_description():
@@ -31,15 +31,7 @@ def generate_launch_description():
         ],
     )
 
-    sim = Node(
-        package='ap1_pnc_sim',
-        executable='pnc_sim_node',
-        name='sim_node',
-        output='screen',
-    )
-
     return LaunchDescription([
         control,
         TimerAction(period=2.0, actions=[planner]),
-        TimerAction(period=4.0, actions=[sim]),
     ])
--- src/bringup/launch/ui_setup.launch.py
+++ src/bringup/launch/ui_setup.launch.py
@@ -3,14 +3,14 @@ from launch_ros.actions import Node
 
 ''' 
 honestly unnecessary because you can just use a ros command to run the interface now using this launch command:
-         ros2 launch ap1_bringup ui_only.launch.py 
+         ros2 launch ap1_bringup ui_setup.launch.py
     if you want to run through regularly just run in another terminal:
-        ros2 run ap1_console system_interface
+        ros2 run ap1_console console
 '''
 def generate_launch_description():
     console = Node(
         package='ap1_console',
-        executable='system_interface',
+        executable='console',
         name='ap1_console',
         output='screen',
     )
